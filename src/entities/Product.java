04c69c4e40db9d860e67bea75f66ce2552d87e91
MarcosDavi <adamsdavi77@gmail.com>
2024-03-26 10:30:15 -0300

    Reference method com método não estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -33,6 +33,10 @@ public class Product {
 		return p.getName().toUpperCase();
 	}
 	
+	public String nonStaticUpperCaseName() {
+		return name.toUpperCase();
+	}
+	
 	@Override
 	public String toString() {
 		return "Product [name = " + name + ", price = " + price + "]";
